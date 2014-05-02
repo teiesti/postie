14bcb08984d9734ca7dff16c9df89b9706443eba
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2014-05-03 01:25:12 +0200

    All methods (except the close()-method in class Postman implemented.

--- src/main/java/de/teiesti/postie/Postman.java
+++ src/main/java/de/teiesti/postie/Postman.java
@@ -2,18 +2,26 @@ package de.teiesti.postie;
 
 import java.net.Socket;
 import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 public abstract class Postman<Letter> implements Runnable, AutoCloseable {
 
 	private Mailbox mailbox;
 	private Class<? extends Letter> letterClass;
 
-	private Set<Recipient> recipients;
+	private Set<Recipient> recipients = new CopyOnWriteArraySet<>();
 
 	private Thread worker;
 
 	public Postman(Socket socket, Class<? extends Letter> letterClass) {
-		// TODO
+		if (letterClass == null)
+			throw new IllegalArgumentException("letterClass == null");
+
+		this.mailbox = new Mailbox(socket);
+		this.letterClass = letterClass;
+		this.worker = new Thread(this);
+
+		worker.start();
 	}
 
 	public /*abstract*/ void send(Letter letter) {
@@ -47,8 +55,9 @@ public abstract class Postman<Letter> implements Runnable, AutoCloseable {
 	@Override
 	public final void run() {
 		try {
+			Letter letter;
 			while (!worker.isInterrupted()) {
-				Letter letter = mailbox.receive(letterClass);
+				letter = mailbox.receive(letterClass);
 				deliver(letter);
 			}
 		} catch(InterruptedException e) {
