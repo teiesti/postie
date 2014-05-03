70372dad89c2147b8aca98b95e5276a124a75d4c
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2014-05-03 02:35:55 +0200

    ParallelPostman implemented.

--- src/main/java/de/teiesti/postie/ParallelPostman.java
+++ src/main/java/de/teiesti/postie/ParallelPostman.java
@@ -1,8 +1,12 @@
 package de.teiesti.postie;
 
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ParallelPostman<Letter> extends Postman<Letter> {
+
+	private ExecutorService es = Executors.newCachedThreadPool();
 
 	public ParallelPostman(Socket socket, Class<? extends Letter> letterClass) {
 		super(socket, letterClass);
@@ -10,7 +14,24 @@ public class ParallelPostman<Letter> extends Postman<Letter> {
 
 	@Override
 	protected void deliver(Letter letter) {
+		for (Recipient r : getRecipients())
+			es.submit(new Worker(r, letter));
+	}
 
+	private class Worker implements Runnable {
+
+		private Recipient<Letter> recipient;
+		private Letter letter;
+
+		public Worker(Recipient<Letter> recipient, Letter letter) {
+			this.recipient = recipient;
+			this.letter = letter;
+		}
+
+		@Override
+		public void run() {
+			recipient.accept(letter);
+		}
 	}
 
 }
