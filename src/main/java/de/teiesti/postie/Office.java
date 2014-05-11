017f0f472090ba1a29bc1503468f7c97e07327ce
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2014-05-11 19:46:27 +0200

    General structure for class Office added.

---
+++ src/main/java/de/teiesti/postie/Office.java
@@ -0,0 +1,23 @@
+package de.teiesti.postie;
+
+import java.net.ServerSocket;
+
+public class Office {
+
+	public Office bind(ServerSocket socket) {
+
+	}
+
+	public Office spawn(Postman postman) {
+
+	}
+
+	public Office start() {
+
+	}
+
+	public Office stop() {
+
+	}
+
+}
