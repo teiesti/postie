3657a4599631b2d3b2fe044bbe4f3978ed0752ce
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2014-05-12 13:57:22 +0200

    Mailbox implemented.

--- src/main/java/de/teiesti/postie/recipients/Mailbox.java
+++ src/main/java/de/teiesti/postie/recipients/Mailbox.java
@@ -3,11 +3,20 @@ package de.teiesti.postie.recipients;
 import de.teiesti.postie.Postman;
 import de.teiesti.postie.Recipient;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class Mailbox<Letter> implements Recipient<Letter> {
+
+    private final BlockingQueue<Letter> inbox = new LinkedBlockingQueue<>();
 
 	@Override
 	public void accept(Letter letter, Postman postman) {
-		// TODO
-	}
+       inbox.add(letter);
+    }
+
+    public Letter receive() throws InterruptedException {
+        return inbox.take();
+    }
 
 }
