3d3fd7d2510d7be61f7507ae422a13c33a20c2b5
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2014-05-13 21:19:50 +0200

    SequentialPostman implemented.

--- src/main/java/de/teiesti/postie/postmen/SequentialPostman.java
+++ src/main/java/de/teiesti/postie/postmen/SequentialPostman.java
@@ -1,13 +1,16 @@
 package de.teiesti.postie.postmen;
 
 import de.teiesti.postie.Postman;
+import de.teiesti.postie.Recipient;
 
 public class SequentialPostman<Letter> extends Postman<Letter> {
 
-	@Override
-	protected Postman deliver(Letter letter) {
-		// TODO
-		return null;
-	}
+    @Override
+    protected Postman<Letter> deliver(Letter letter) {
+        for (Recipient r : recipients)
+            r.accept(letter, this);
+
+        return this;
+    }
 
 }
