e03772c8f0331476290578f878cb497e56625aaa
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2014-05-18 16:02:15 +0200

    Javadoc for Recipient added.

--- src/main/java/de/teiesti/postie/Recipient.java
+++ src/main/java/de/teiesti/postie/Recipient.java
@@ -1,7 +1,19 @@
 package de.teiesti.postie;
 
+/**
+ * A {@link Recipient} accepts {@link Letter}s from one or more {@link Postman}.
+ *
+ * @param <Letter> type of the letters
+ */
 public interface Recipient<Letter> {
 
+	/**
+	 * Accepts a given {@link Letter} from a given {@link Postman}. This method does something useful with the given
+	 * {@link Letter}.
+	 *
+	 * @param letter the {@link Letter}
+	 * @param postman the {@link Postman} that delivered the {@link Letter}
+	 */
 	public void accept(Letter letter, Postman postman);
 
 }
