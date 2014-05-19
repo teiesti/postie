b4ed0a27a4a4be4de89a97b0c534b9bfa9f67370
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2014-05-19 20:44:58 +0200

    Javadoc for SemiParallelPostman added.

--- src/main/java/de/teiesti/postie/postmen/SemiParallelPostman.java
+++ src/main/java/de/teiesti/postie/postmen/SemiParallelPostman.java
@@ -9,11 +9,31 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 // TODO doc: difference between ParallelPostman and SemiParallelPostman: the last sequentializes the letters!
+
+/**
+ * A {@link SemiParallelPostman} is a {@link Postman} that delivers {@link Letter}s in parallel but with care for the
+ * {@link Letter} order. This means that a {@link SemiParallelPostman} - in contrast to a {@link ParallelPostman} -
+ * waits until any {@link Recipient#accept} has return before the next {@link Letter} is processed. A
+ * {@link SemiParallelPostman} creates a {@link Runnable} for each pair of received {@link Letter} and registered
+ * {@link Recipient}. The {@link Runnable}s are submitted to an {@link ExecutorService}.
+ *
+ * @param <Letter> type of the letters
+ */
 public class SemiParallelPostman<Letter> extends Postman<Letter> {
 
     private ExecutorService es = Executors.newCachedThreadPool();
     private CountDownLatch cdl = new CountDownLatch(0);
 
+	/**
+	 * Delivers a given {@link Letter} in parallel using an {@link ExecutorService}. This method creates a {@link
+	 * Runnable} for each {@link Recipient} and submits it to the {@link ExecutorService}. This method blocks until
+	 * the {@link Letter} from the the preceding call is completely executed. This guarantes the {@link Letter} order
+	 * is not mixed up.
+	 *
+	 * @param letter the {@link Letter} to deliver
+	 *
+	 * @return this {@link Postman}
+	 */
     @Override
     protected Postman<Letter> deliver(Letter letter) {
         try {
